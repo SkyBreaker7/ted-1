82d995c5d384ccea027c00da688eee4de06a9e2e
Emeric54 <fischer.emeric@gmail.com>
2016-01-17 17:53:37 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'sqlite3-ruby', '1.3.3', :group => :development
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ted::Application.config.secret_token = 'a845d832d14b40b98883ed5566a12b5a42489332aac650213c6042fe394254151ef67e843b1837bca19cbf7b2e5d1fe19f9f988df488b959e77d2fcae92003c3'
